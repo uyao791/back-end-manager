8f4df04f0860cbb0aa6c72cb0b91e198fde57d13
1781012959@qq.com <2014liyao>
2021-10-08 15:15:47 +0800

    sql

--- src/main/resources/sql/base_admin_role.sql
+++ src/main/resources/sql/base_admin_role.sql
@@ -1,41 +1,37 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : 线下服务器
- Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : 172.16.1.60:3306
- Source Schema         : tiantue
+Source Server         : 192.168.64.130
+Source Server Version : 50735
+Source Host           : 192.168.64.130:3306
+Source Database       : backend_manager
 
- Target Server Type    : MySQL
- Target Server Version : 50721
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50735
+File Encoding         : 65001
 
- Date: 05/12/2018 10:56:17
+Date: 2021-10-08 15:15:13
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for base_admin_role
 -- ----------------------------
 DROP TABLE IF EXISTS `base_admin_role`;
-CREATE TABLE `base_admin_role`  (
+CREATE TABLE `base_admin_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '权限角色ID',
-  `role_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名称',
-  `role_desc` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '角色描述',
-  `permissions` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '权限',
-  `create_time` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '创建时间',
-  `update_time` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '更新时间',
-  `role_status` int(1) NOT NULL DEFAULT 1 COMMENT '1：有效 \r\n            0：无效',
+  `role_name` varchar(30) NOT NULL COMMENT '角色名称',
+  `role_desc` varchar(100) DEFAULT NULL COMMENT '角色描述',
+  `permissions` varchar(20) DEFAULT NULL COMMENT '权限',
+  `create_time` varchar(64) DEFAULT NULL COMMENT '创建时间',
+  `update_time` varchar(64) DEFAULT NULL COMMENT '更新时间',
+  `role_status` int(1) NOT NULL DEFAULT '1' COMMENT '1：有效 \r\n            0：无效',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户角色表' ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='系统用户角色表';
 
 -- ----------------------------
 -- Records of base_admin_role
 -- ----------------------------
-INSERT INTO `base_admin_role` VALUES (1, '系统管理员', '系统管理员', '1,9', '2018-11-21 15:54:07', '2018-11-21 15:54:07', 1);
-INSERT INTO `base_admin_role` VALUES (2, '普通管理员', '普通管理员', '9', '2018-11-21 15:11:44', '2018-12-03 19:09:57', 1);
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `base_admin_role` VALUES ('1', 'system manager', '系统管理员', '1,9', '2018-11-21 15:54:07', '2018-11-21 15:54:07', '1');
+INSERT INTO `base_admin_role` VALUES ('2', 'base manager', '普通管理员', '9', '2018-11-21 15:11:44', '2018-12-03 19:09:57', '1');
--- src/main/resources/sql/base_admin_user.sql
+++ src/main/resources/sql/base_admin_user.sql
@@ -1,42 +1,38 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : 线下服务器
- Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : 172.16.1.60:3306
- Source Schema         : tiantue
+Source Server         : 192.168.64.130
+Source Server Version : 50735
+Source Host           : 192.168.64.130:3306
+Source Database       : backend_manager
 
- Target Server Type    : MySQL
- Target Server Version : 50721
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50735
+File Encoding         : 65001
 
- Date: 05/12/2018 10:56:07
+Date: 2021-10-08 15:15:22
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for base_admin_user
 -- ----------------------------
 DROP TABLE IF EXISTS `base_admin_user`;
-CREATE TABLE `base_admin_user`  (
+CREATE TABLE `base_admin_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `sys_user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '系统用户名称',
-  `sys_user_pwd` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '系统用户密码',
+  `sys_user_name` varchar(50) NOT NULL COMMENT '系统用户名称',
+  `sys_user_pwd` varchar(250) DEFAULT NULL COMMENT '系统用户密码',
   `role_id` int(255) DEFAULT NULL COMMENT '角色',
-  `user_phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号',
-  `reg_time` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登记时间',
-  `user_status` int(1) NOT NULL DEFAULT 0 COMMENT '状态（0：无效；1：有效）',
+  `user_phone` varchar(11) NOT NULL COMMENT '手机号',
+  `reg_time` varchar(32) NOT NULL COMMENT '登记时间',
+  `user_status` int(1) NOT NULL DEFAULT '0' COMMENT '状态（0：无效；1：有效）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统管理员帐号' ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='系统管理员帐号';
 
 -- ----------------------------
 -- Records of base_admin_user
 -- ----------------------------
-INSERT INTO `base_admin_user` VALUES (1, 'admin', '3ef7164d1f6167cb9f2658c07d3c2f0a', 1, '13411182215', '2018-11-22 10:57:33', 1);
-INSERT INTO `base_admin_user` VALUES (2, 'jackson', '6565673a6caee66a6acbd51415bddbda', 2, '19563648695', '2018-11-22 10:57:33', 1);
-INSERT INTO `base_admin_user` VALUES (4, 'alice', '5e1030d25f5ca46aac4c0369b908d762', 2, '11111111111', '2018-11-22 11:01:58', 1);
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `base_admin_user` VALUES ('1', 'admin', '3ef7164d1f6167cb9f2658c07d3c2f0a', '1', '13411182215', '2018-11-22 10:57:33', '1');
+INSERT INTO `base_admin_user` VALUES ('2', 'jackson', '6f055494278cc66b5f46a684fc1276c7', '2', '19563648695', '2018-11-22 10:57:33', '1');
+INSERT INTO `base_admin_user` VALUES ('4', 'alice', '5e1030d25f5ca46aac4c0369b908d762', '2', '11111111111', '2018-11-22 11:01:58', '1');
